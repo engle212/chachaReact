b7d1733cc962d38d0469ad733ad0807f7ea0d6c1
John Engle <englejohn64@gmail.com>
2025-02-24 16:07:11 -0500

    Separated article page from index page

--- src/app/page.js
+++ src/app/page.js
@@ -3,32 +3,11 @@ export const dynamic = 'force-dynamic';
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-const BASE_URL = "https://api.json-generator.com";
-const API_URL = BASE_URL + "/templates/L0Wo66RzmkOi/data";
-
-export default function Page() {
-  const [article, setArticle] = useState({})
-
-  useEffect(() => {
-    axios.get(API_URL, {
-      headers: {
-        Authorization: "Bearer qbxqx0vjd62pmwgi1eu31edj6kxqrshefotxgqso"
-      }
-    }).then(function (response) {
-      setArticle(response.data[0])
-      console.log(article);
-    })
-    .catch(function (error) {
-      console.error("Error fetching article", error);
-    })
-  }, []);
+export default function IndexPage() {
+  
   return (
     <div>
-      <h1>{article.title}</h1>
-      <p>{article.author}</p>
-      <p>{article.source}</p>
-      <p>{article.pubDate}</p>
-      <p>{article.content}</p>
+      <h1>Index!</h1>
     </div>
   );
 }
